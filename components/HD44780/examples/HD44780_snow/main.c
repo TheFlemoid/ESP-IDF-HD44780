58ce3a12ca0681e7e50f1ad2fd95148021a993b1
TheFlemoid <trey.dahlberg@gmail.com>
2025-02-10 23:56:19 -0600

    Refactoring repo to be a proper ESP-IDF component.

---
+++ components/HD44780/examples/HD44780_snow/main.c
@@ -0,0 +1,42 @@
+/**
+ * File:       main.c
+ * Author:     Franklyn Dahlberg
+ * Created:    10 February, 2025
+ * Copyright:  2025  Franklyn Dahlberg
+ * License:    MIT License (see https://choosealicense.com/licenses/mit/)
+ */
+
+/**
+ * Prints two lines of inverted * characters to make a crude snow animation.
+ * Designed to show basic HD44780 functionality.
+ */
+#include "HD44780.h"
+#include "freertos/FreeRTOS.h"
+
+void app_main(void)
+{
+    HD44780_FOUR_BIT_BUS bus = { 18, 19, 21, 22, 16, 17 }; 
+
+    HD44780_initFourBitBus(&bus);
+
+    bool pattern = false;
+
+    while (true) {
+        HD44780_clear();
+
+        for (int i = 0; i < 16; i+=2) {
+            HD44780_setCursorPos(i, (pattern == false));
+            HD44780_write("*");
+        }
+
+        for (int i = 1; i < 16; i+=2) {
+            HD44780_setCursorPos(i, (pattern == true));
+            HD44780_write("*");
+        }
+
+        pattern = !pattern;
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
